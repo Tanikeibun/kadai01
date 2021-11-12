78e04cb641e41b401da224bebbfd1b7871ccd2d0
20jy0221 <20jy0221@PC07.jynet2.jec.ac.jp>
2021-11-12 14:06:44 +0900

    komi2

--- src/HelloGitHub.java
+++ src/HelloGitHub.java
@@ -4,6 +4,7 @@ public class HelloGitHub {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("Hello GitHub");
+		System.out.println("Hello GitHub1");
 	}
 
 }
